128227aea2743ac8b2285bd61010c1fe245f80f5
knarf <hejun1874@163.com>
2016-04-25 10:39:55 +0800

    Optimize framework

---
+++ app/hia/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+from config import Config
+
+db = MongoEngine()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+
+    return app
---
+++ app/manage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import os
+
+from flask_script import Manager, Server
+
+from hia import create_app, db
+
+app = create_app(os.getenv('config') or 'default')
+manager = Manager(app)
+
+# Turn os debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0',
+    port = 5000
+))
+
+if __name__ == "__main__":
+    manager.run()
