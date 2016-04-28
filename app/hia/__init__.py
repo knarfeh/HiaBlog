e10a53771a265481e9de4c68a7b265af694027b2
knarfeh <knarfeh@outlook.com>
2016-04-28 20:24:07 +0800

    Add login/logout

--- app/hia/__init__.py
+++ app/hia/__init__.py
@@ -3,24 +3,31 @@
 
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_login import LoginManager
 
-from config import Config
+from config import config
 
 db = MongoEngine()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'accounts.login'
 
 
 def create_app(config_name):
     app = Flask(__name__,
-                template_folder=Config.TEMPLATE_PATH,
-                static_folder=Config.STATIC_PATH)
+                template_folder=config[config_name].TEMPLATE_PATH,
+                static_folder=config[config_name].STATIC_PATH)
 
-    app.config.from_object(Config)
+    app.config.from_object(config[config_name])
 
-    Config.init_app(app)
+    config[config_name].init_app(app)
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from main.urls import main as main_blueprint
+    from accounts.urls import accounts as accounts_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(accounts_blueprint, url_prefix='/accounts')
 
     return app
