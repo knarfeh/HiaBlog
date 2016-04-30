e38ea5887e3bb55e5dae550059aeba17abbb3da2
knarfeh <knarfeh@outlook.com>
2016-04-30 23:09:55 +0800

    Optimize

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -12,8 +12,8 @@ class User(UserMixin, db.Document):
     username = db.StringField(max_length=255, required=True)
     email = db.EmailField(max_length=255)
     password_hash = db.StringField(required=True)
-    create_time = db.DateTimeField(default=datetime.datetime.now, required=True)
-    last_login = db.DateTimeField(default=datetime.datetime.now, required=True)
+    create_time = db.DateTimeField(default=datetime.datetime.now(), required=True)
+    last_login = db.DateTimeField(default=datetime.datetime.now(), required=True)
 
     @property
     def password(self):
