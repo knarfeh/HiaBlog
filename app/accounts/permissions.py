1e8409962a46042ff91751ee5045d3ead7c5cc75
knarfeh <knarfeh@outlook.com>
2016-05-02 23:43:55 +0800

    Add posts permission control

--- app/accounts/permissions.py
+++ app/accounts/permissions.py
@@ -5,7 +5,10 @@ from flask import current_app
 from flask_principal import Permission, RoleNeed, UserNeed, identity_loaded
 from flask_login import current_user
 
-admin_permission = Permission(RoleNeed('admin'))
+su_need = RoleNeed('su')
+
+su_permission = Permission(su_need)
+admin_permission = Permission(RoleNeed('admin')).union(su_permission)
 editor_permission = Permission(RoleNeed('editor')).union(admin_permission)
 writer_permission = Permission(RoleNeed('writer')).union(editor_permission)
 reader_permission = Permission(RoleNeed('reader')).union(writer_permission)
@@ -24,5 +27,8 @@ def on_identity_loaded(sender, identity):
     # identity with the roles that the user provides
     if hasattr(current_user, 'role'):
         # for role in current_user.roles:
+        if current_user.is_superuser:
+            identity.provides.add(su_need)
+
         identity.provides.add(RoleNeed(current_user.role))
 
